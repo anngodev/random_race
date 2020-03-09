1d4fdd50a8b5b1733ecbdf4af65f7ff658e415ef
Andy <ngoan_230@yahoo.com>
2020-03-09 14:59:16 -0400

    Animation for car 1

--- javascript/ranndom_race.js
+++ javascript/ranndom_race.js
@@ -11,14 +11,28 @@ $("#go").click(function() {
     var raceTrackWidth = $(window).width() - carWidth;
     
     // GENERATE RANDOM NUMBER BETWEEN 1 AND 5000 (milliseconds)
-    var raceTime1 = Math.floor( (Math.random() * 5000) + 1 + "seconds.");
+    var raceTime1 = Math.floor( (Math.random() * 5000) + 1);
     
-    var raceTime2 = Math.floor( (Math.random() * 5000) + 1 + "seconds.");
+    var raceTime2 = Math.floor( (Math.random() * 5000) + 1);
                                
     // SET A FLAG VARIABLE TO FALSE BY DEFAULT
     var isComplete = false;
                                
     // SET FLAG VARIABLE TO FIRST BY DEFAULT
     var place = "first";
+    
+    // ANIMATE CAR 1
+    $("#car1").animate({
+       // MOVE THE CAR THE WIDTH OF THE RACETRACK
+        left: raceTrackWidth,
+        
+        
+    }, raceTime1, function() {
+        
+        // ANIMATION IS COMPLETE
+        
+        // RUN FUNCTION
+        
+    });
                                
 });
